0a6e6fa26886c00870fca02f9ec5e3b34f3fe1d2
aisarthak <aisarthak2@gmail.com>
2023-12-04 19:41:32 +0530

    navbar setup complete
    setup navbar animation tooltip , started page transition

--- components/Transition.js
+++ components/Transition.js
@@ -1,7 +1,32 @@
+import { motion } from "framer-motion";
+
+
+
+
+const transitionVariants = {
+  initial: {
+    x: '100%',
+    width: '100%',
+  },
+  animate: {
+    x: '0%',
+    width: '0%',
+  },
+
+  exit :{
+    x: ['0%', '100%'],
+    width: ['0%', '100%'],
+  }
+}
+
+
+
 const Transition = () => {
   return (
     <>
-      <div>Transition</div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
     </>
   );
 };
